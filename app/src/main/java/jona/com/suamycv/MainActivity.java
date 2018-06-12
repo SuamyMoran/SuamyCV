87751b445a9abf4b71e71bd5d831103bb1a32023
Jonathan Mullo <tristhan1993@hotmail.com>
2018-06-11 20:41:32 -0500

    principio
    creacion del proyecto android y agregar algunos styles para usar mas
    adelante

---
+++ app/src/main/java/jona/com/suamycv/MainActivity.java
@@ -0,0 +1,13 @@
+package jona.com.suamycv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
